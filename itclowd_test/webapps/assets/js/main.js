619426c0726348ce36cb700074b51a0abd09d515
DELL <DELL@DESKTOP-RNVBF3L>
2018-01-31 13:24:32 +0900

    맘껏 넣어보시오

--- itclowd_test/webapps/assets/js/main.js
+++ itclowd_test/webapps/assets/js/main.js
@@ -23,13 +23,13 @@
 			$main = $('#main');
 
 		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+			/*$body.addClass('is-loading');
 
 			$window.on('load', function() {
 				window.setTimeout(function() {
 					$body.removeClass('is-loading');
 				}, 100);
-			});
+			});*/
 
 		// Fix: Placeholder polyfill.
 			$('form').placeholder();
